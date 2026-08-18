5e704b743afd1c1d3bfce716dafafc7c7a82536c
Prasad <prasad.project2025@gmail.com>
2026-08-18 08:48:26 +0530

    Time: 1 ms (99.3%), Space: 44.3 MB (94.39%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,24 +1,51 @@
 class Solution {
-  public int largestInteger(int[] nums, int k) {
-    if (k == nums.length)
-      return Arrays.stream(nums).max().getAsInt();
-    int[] count = getCount(nums);
-    if (k == 1)
-      return maxUnique(nums, count);
-    return Math.max(count[nums[0]] == 1 ? nums[0] : -1,
-                    count[nums[nums.length - 1]] == 1 ? nums[nums.length - 1] : -1);
-  }
+    static {
+        for(int i = 0; i <= 500; i++) {
+            largestInteger(new int[0], 0);
+        }
+    }
+    public static int largestInteger(int[] nums, int k) {
+        int n = nums.length;
+        if(n == 0) return n;
+        // Case 2, find maximum num
+        if(n == k) {
+            int max = nums[0];
+            for(int i = 1; i < n; i++) {
+                max = Math.max(max, nums[i]);
+            }
+            return max;
+        }
+        // Case 3, find maximum with freq 1
+        if(k == 1) {
+            // Better than a HashMap here lol
+            int[] freq = new int[51];
+            for(int c : nums) {
+                freq[c]++;
+            }
+            int max = -1;
+            for(int c : nums) {
+                if(freq[c] >= 2) continue;
+                max = Math.max(max, c);
+            }
+            return max;
+        }
+        // Case 1, either nums[0] or nums[n-1]
+        // Check if they appear once
+        if(nums[0] == nums[n-1]) return -1;
+        boolean option1 = true;
+        boolean option2 = true;
+        for(int i = 1; i < n-1; i++) {
+            if(nums[i] == nums[0]) option1 = false;
+            if(nums[i] == nums[n-1]) option2 = false;
+        }
+        if(option1 && !option2) return nums[0];
+        if(!option1 && option2) return nums[n-1];
+        if(!option1 && !option2) return -1;
+        return Math.max(nums[0], nums[n-1]);
+    }
 
-  // Returns the maximum unique integer in nums if any. Otherwise, returns -1.
-  private int maxUnique(int[] nums, int[] count) {
-    return Arrays.stream(nums).filter(num -> count[num] == 1).max().orElse(-1);
-  }
-
-  private int[] getCount(int[] nums) {
-    final int MAX = 50;
-    int[] count = new int[MAX + 1];
-    for (final int num : nums)
-      ++count[num];
-    return count;
-  }
+    // Intuition: x can only appear 1 time in subarrays of size k, if:
+    // - It's at index 0 or index n-1 AND only appears once
+    // - k == n
+    // - k == 1 -> Find maximum single freq
 }
