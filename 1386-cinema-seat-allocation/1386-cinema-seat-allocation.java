20d4b4e7f65c42544a3b39a8224725ea1fca0c0f
Prasad <prasad.project2025@gmail.com>
2026-08-19 09:53:01 +0530

    Time: 15 ms (98.68%), Space: 54 MB (25.26%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -1,25 +1,20 @@
 class Solution {
-  public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
-    int ans = 0;
-    Map<Integer, Integer> rowToSeats = new HashMap<>();
-
-    for (int[] reservedSeat : reservedSeats) {
-      final int row = reservedSeat[0];
-      final int seat = reservedSeat[1];
-      rowToSeats.put(row, rowToSeats.getOrDefault(row, 0) | 1 << (seat - 1));
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+        Map<Integer, Integer> d = new HashMap<>();
+        for (var e : reservedSeats) {
+            int i = e[0], j = e[1];
+            d.merge(i, 1 << (10 - j), (x, y) -> x | y);
+        }
+        int[] masks = {0b0111100000, 0b0000011110, 0b0001111000};
+        int ans = (n - d.size()) * 2;
+        for (int x : d.values()) {
+            for (int mask : masks) {
+                if ((x & mask) == 0) {
+                    x |= mask;
+                    ++ans;
+                }
+            }
+        }
+        return ans;
     }
-
-    for (final int seats : rowToSeats.values())
-      if ((seats & 0b0111111110) == 0)
-        // Can fit 2 four-person groups.
-        ans += 2;
-      else if ((seats & 0b0111100000) == 0 || // The left is not occupied.
-               (seats & 0b0001111000) == 0 || // The middle is not occupied.
-               (seats & 0b0000011110) == 0)   // The right is notoccupied.
-        // Can fit 1 four-person group.
-        ans += 1;
-
-    // Any empty row can fit 2 four-person groups.
-    return ans + (n - rowToSeats.size()) * 2;
-  }
 }
