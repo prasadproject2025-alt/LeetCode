bd3cc6dbebffcf77fa0e13f4b06af8c97348c130
Prasad <prasad.project2025@gmail.com>
2026-08-23 10:48:00 +0530

    Time: 5 ms (100%), Space: 47 MB (64.23%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -1,18 +1,28 @@
 class Solution {
-  public boolean sumGame(String num) {
-    final int n = num.length();
-    double ans = 0.0;
 
-    for (int i = 0; i < n / 2; ++i)
-      ans += getExpectation(num.charAt(i));
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int[] left = get(num.substring(0, n / 2));
+        int[] right = get(num.substring(n / 2, n));
 
-    for (int i = n / 2; i < n; ++i)
-      ans -= getExpectation(num.charAt(i));
+        int n0 = left[0],
+            q0 = left[1];
+        int n1 = right[0],
+            q1 = right[1];
 
-    return ans != 0.0;
-  }
+        return (q0 + q1) % 2 == 1 || n0 - n1 != ((q1 - q0) * 9) / 2;
+    }
 
-  private double getExpectation(char c) {
-    return c == '?' ? 4.5 : c - '0';
-  }
+    private int[] get(String s) {
+        int nn = 0,
+            qq = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == '?') {
+                qq++;
+            } else {
+                nn += ch - '0';
+            }
+        }
+        return new int[] { nn, qq };
+    }
 }
