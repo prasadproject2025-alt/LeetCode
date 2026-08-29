e56aff993197baa895ea64c03eaf1e27235c8424
Prasad <prasad.project2025@gmail.com>
2026-08-29 19:17:51 +0530

    Time: 45 ms (99.24%), Space: 141.2 MB (80.3%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
@@ -1,43 +1,48 @@
+
+import java.util.Arrays;
+
 class Solution {
-  public int[] lexicographicallySmallestArray(int[] nums, int limit) {
-    int[] ans = new int[nums.length];
-    List<List<Pair<Integer, Integer>>> numAndIndexesGroups = new ArrayList<>();
+    public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+        int n = nums.length;
+        long[] sorted = new long[n];
 
-    for (Pair<Integer, Integer> numAndIndex : getNumAndIndexes(nums))
-      if (numAndIndexesGroups.isEmpty() ||
-          numAndIndex.getKey() -
-                  numAndIndexesGroups.get(numAndIndexesGroups.size() - 1)
-                      .get(numAndIndexesGroups.get(numAndIndexesGroups.size() - 1).size() - 1)
-                      .getKey() >
-              limit) {
-        // Start a new group.
-        numAndIndexesGroups.add(new ArrayList<>(List.of(numAndIndex)));
-      } else {
-        // Append to the existing group.
-        numAndIndexesGroups.get(numAndIndexesGroups.size() - 1).add(numAndIndex);
-      }
+        for (int i = 0; i < n; i++) {
+            sorted[i] = ((long) nums[i] << 32) | (i & 0xffffffffL);
+        }
 
-    for (List<Pair<Integer, Integer>> numAndIndexesGroup : numAndIndexesGroups) {
-      List<Integer> sortedNums = new ArrayList<>();
-      List<Integer> sortedIndices = new ArrayList<>();
-      for (Pair<Integer, Integer> pair : numAndIndexesGroup) {
-        sortedNums.add(pair.getKey());
-        sortedIndices.add(pair.getValue());
-      }
-      sortedIndices.sort(null);
-      for (int i = 0; i < sortedNums.size(); ++i) {
-        ans[sortedIndices.get(i)] = sortedNums.get(i);
-      }
+        Arrays.sort(sorted);
+
+        int[] values = new int[n];
+        for (int i = 0; i < n; i++) {
+            values[i] = (int) (sorted[i] >>> 32);
+        }
+
+        int start = 0;
+
+        while (start < n) {
+            int end = start;
+
+            while (end + 1 < n &&
+                   values[end + 1] - values[end] <= limit) {
+                end++;
+            }
+
+            int size = end - start + 1;
+            int[] indices = new int[size];
+
+            for (int i = 0; i < size; i++) {
+                indices[i] = (int) sorted[start + i];
+            }
+
+            Arrays.sort(indices);
+
+            for (int i = 0; i < size; i++) {
+                nums[indices[i]] = values[start + i];
+            }
+
+            start = end + 1;
+        }
+
+        return nums;
     }
-
-    return ans;
-  }
-
-  private Pair<Integer, Integer>[] getNumAndIndexes(int[] nums) {
-    Pair<Integer, Integer>[] numAndIndexes = new Pair[nums.length];
-    for (int i = 0; i < nums.length; ++i)
-      numAndIndexes[i] = new Pair<>(nums[i], i);
-    Arrays.sort(numAndIndexes, Comparator.comparingInt(Pair::getKey));
-    return numAndIndexes;
-  }
 }
